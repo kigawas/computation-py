48bddb7c8e1f0e55d74d5f1653c4bae63f0fe974
Lumit <to.be.impressive@gmail.com>
2016-04-12 15:18:21 +0800

    Finish DFA

---
+++ src/automata/farule.py
@@ -0,0 +1,19 @@
+from __future__ import print_function, unicode_literals
+
+
+class FARule(object):
+    def __init__(self, state, character, next_state):
+        self.state = state
+        self.character = character
+        self.next_state = next_state
+
+    def __str__(self):
+        return '#<FARule {} --{}--> {}'.format(self.state, self.character,
+                                               self.next_state)
+
+    def applies_to(self, state, character):
+        return self.state == state and self.character == character
+
+    @property
+    def follow(self):
+        return self.next_state
---
+++ src/automata/utils.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, unicode_literals
+
+import unittest
+
+
+def detect(arr, func):
+    for i in arr:
+        if func(i):
+            return i
+
+
+class UtilTest(unittest.TestCase):
+    def test_detect(self):
+        self.assertEqual(detect([1, 2, 3], lambda x: x > 1 and x < 3), 2)
+        self.assertEqual(detect([1, 2, 3], lambda x: False), None)
+        self.assertEqual(detect([1, 2, 3], lambda x: True), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
