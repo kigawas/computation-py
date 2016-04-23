4327dd8baa6198affd9919f0c1dfead97aa0ff18
Weiliang Li <to.be.impressive@gmail.com>
2016-04-23 12:35:45 +0800

    Test generated Python code's evaluation

--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -2,6 +2,7 @@ from __future__ import print_function, unicode_literals
 
 from expressions import Add, Boolean, Multiply, LessThan, Number, Variable
 from statements import Assign, If, Sequence, While
+from utils import merge_dict
 
 
 class Machine(object):
@@ -32,10 +33,11 @@ def test():
 
     seq = While(
         LessThan(
-            Variable('x'), Number(5)), Assign('x', Multiply(
+            Variable('x'), Number(50)), Assign('x', Add(
                 Variable('x'), Number(3))))
     Machine(seq, {'x': Number(1)}).run()
     print(seq.to_python)
+    print(eval(seq.to_python)({'x': 1}))
 
 
 if __name__ == '__main__':
