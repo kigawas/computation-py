7adf082b5cf007d57bb07a0ab3fb11f8c6500eb2
Weiliang Li <to.be.impressive@gmail.com>
2016-05-02 13:52:17 +0800

    Use pure python to generate Assign

--- src/interpreter/utils.py
+++ src/interpreter/utils.py
@@ -1,22 +1,21 @@
 from __future__ import print_function, unicode_literals
+from itertools import chain
 
 import unittest
 
 
 def merge_dict(dic1, dic2):
     '''Merge dic2 to dic1 without changing dic1'''
-    dm = {}
-    for k, v in dic1.iteritems():
-        dm[k] = v
-    for k, v in dic2.iteritems():
-        dm[k] = v
-    return dm
+    return {k: v for d in (dic1, dic2) for k, v in d.items()}
 
 
 class UtilTest(unittest.TestCase):
     def test_merge_dict(self):
         d1 = {'a': 1, 'b': 2, 'c': 3}
         d2 = {'a': 2}
+
+        self.assertEqual(
+            merge_dict(d1, d2), dict(chain(d1.items(), d2.items())))
         self.assertEqual(merge_dict(d1, d2), {'a': 2, 'b': 2, 'c': 3})
 
 
