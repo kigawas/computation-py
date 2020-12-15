6c3abb34b39d4ca749122bc60c1f8172512dfbfe
Weiliang Li <to.be.impressive@gmail.com>
2020-12-15 10:34:22 +0900

    Parser (#4)
    * refactor expressions
    
    * use lark as parser
    
    * pytest

--- tests/interpreter/test_expr.py
+++ tests/interpreter/test_expr.py
@@ -1,5 +1,3 @@
-import unittest
-
 from computation.interpreter.expressions import (
     Add,
     Boolean,
@@ -10,70 +8,65 @@ from computation.interpreter.expressions import (
 )
 
 
-class TypeTest(unittest.TestCase):
-    def test_number(self):
-        self.assertEqual(Number(3), Number(3))
-        self.assertNotEqual(Number(3), Number(4))
-        self.assertLess(Number(2), Number(4))
-        self.assertGreater(Number(4), Number(2))
-
-    def test_boolean(self):
-        self.assertTrue(Boolean(True) and Boolean(True))
-        self.assertEqual(Boolean(True), Boolean(True))
-        self.assertEqual(Boolean(False), Boolean(False))
-        self.assertNotEqual(Boolean(True), Boolean(False))
+def test_number():
+    assert Number(3) == Number(3)
+    assert Number(3) != Number(4)
+    assert Number(2) < Number(4)
+    assert Number(4) > Number(2)
 
 
-class ExprTest(unittest.TestCase):
-    def test_add(self):
-        expr = Add(Variable("x"), Variable("y"))
-        en = {"x": Number(1), "y": Number(2)}
-        while expr.reducible:
-            expr = expr.reduce(en)
-        self.assertEqual(expr, Number(3))
-
-    def test_mul(self):
-        expr = Multiply(Variable("x"), Variable("y"))
-        en = {"x": Number(3), "y": Number(4)}
-        while expr.reducible:
-            expr = expr.reduce(en)
-        self.assertEqual(expr, Number(12))
-
-    def test_less(self):
-        expr = LessThan(Variable("x"), Variable("y"))
-        en = {"x": Number(1), "y": Number(3)}
-        while expr.reducible:
-            expr = expr.reduce(en)
-        self.assertEqual(expr, Boolean(True))
+def test_boolean():
+    assert Boolean(True) and Boolean(True)
+    assert Boolean(True), Boolean(True)
+    assert Boolean(False), Boolean(False)
+    assert Boolean(True) != Boolean(False)
 
 
-class EvalTest(unittest.TestCase):
-    def test_type(self):
-        self.assertEqual(Number(23).evaluate({}), Number(23))
-        self.assertEqual(Variable("x").evaluate({"x": Number(23)}), Number(23))
-        self.assertEqual(
-            LessThan(Add(Variable("x"), Number(2)), Variable("y")).evaluate(
-                {"x": Number(2), "y": Number(5)}
-            ),
-            Boolean(True),
-        )
+def test_add():
+    expr = Add(Variable("x"), Variable("y"))
+    en = {"x": Number(1), "y": Number(2)}
+    while expr.reducible:
+        expr = expr.reduce(en)
+    assert expr == Number(3)
 
 
-class CodeGenTest(unittest.TestCase):
-    def test_type(self):
-        n1 = Number(5)
-        b1 = Boolean(False)
-        v1 = Variable("x")
+def test_mul():
+    expr = Multiply(Variable("x"), Variable("y"))
+    en = {"x": Number(3), "y": Number(4)}
+    while expr.reducible:
+        expr = expr.reduce(en)
+    assert expr == Number(12)
 
-        self.assertEqual(eval(n1.to_python)({}), 5)
-        self.assertEqual(eval(b1.to_python)({}), False)
-        self.assertEqual(eval(v1.to_python)({"x": 7}), 7)
 
-    def test_expr(self):
-        a1 = Add(Variable("x"), Number(1))
-        m1 = Multiply(Variable("x"), Number(9))
-        l1 = LessThan(Variable("x"), Variable("y"))
+def test_less():
+    expr = LessThan(Variable("x"), Variable("y"))
+    en = {"x": Number(1), "y": Number(3)}
+    while expr.reducible:
+        expr = expr.reduce(en)
+    assert expr == Boolean(True)
 
-        self.assertEqual(eval(a1.to_python)({"x": 7}), 8)
-        self.assertEqual(eval(m1.to_python)({"x": 9}), 81)
-        self.assertEqual(eval(l1.to_python)({"x": 7, "y": 8}), True)
+
+def test_type():
+    assert Number(23).evaluate({}) == Number(23)
+    assert Variable("x").evaluate({"x": Number(23)}) == Number(23)
+    assert LessThan(Add(Variable("x"), Number(2)), Variable("y")).evaluate(
+        {"x": Number(2), "y": Number(5)}
+    )
+
+    n1 = Number(5)
+    b1 = Boolean(False)
+    v1 = Variable("x")
+
+    assert eval(n1.to_python)({}) == 5
+    assert not eval(b1.to_python)({})
+    assert eval(v1.to_python)({"x": 7}) == 7
+
+
+def test_expr():
+    a1 = Add(Variable("x"), Number(1))
+    m1 = Multiply(Variable("x"), Number(9))
+    l1 = LessThan(Variable("x"), Variable("y"))
+
+    assert eval(a1.to_python)({"x": 7}) == 8
+    assert eval(m1.to_python)({"x": 9}) == 81
+    assert eval(l1.to_python)({"x": 7, "y": 8})
--- tests/interpreter/test_interpreter.py
+++ tests/interpreter/test_interpreter.py
@@ -1,5 +1,3 @@
-import unittest
-
 from computation.interpreter.interpreter import Machine
 from computation.interpreter.expressions import (
     Add,
@@ -8,37 +6,52 @@ from computation.interpreter.expressions import (
     Multiply,
     LessThan,
 )
-from computation.interpreter.statements import Sequence, Assign, While
+from computation.interpreter.statements import DoNothing, Sequence, Assign, While
+from computation.interpreter.parser import parse
 
 
-class InterpreterTest(unittest.TestCase):
-    def test_interpreter(self):
-        SEP = "=" * 50
-        seq = Assign("x", Add(Variable("x"), Number(1)))
-        Machine(seq, {"x": Number(5)}, True).run()
+def test_interpreter():
+    SEP = "=" * 50
+    seq = Assign("x", Add(Variable("x"), Number(1)))
+    Machine(seq, {"x": Number(5)}, True).run()
 
-        print(SEP)
+    print(SEP)
 
-        seq = Sequence(
-            Assign("x", Add(Number(1), Number(1))),
-            Assign("y", Multiply(Variable("x"), Number(2))),
-        )
-        Machine(seq, {}, True).run()
+    seq = Sequence(
+        Assign("x", Add(Number(1), Number(1))),
+        Assign("y", Multiply(Variable("x"), Number(2))),
+    )
+    Machine(seq, {}, True).run()
 
-        print(SEP)
+    print(SEP)
 
-        seq = Sequence(
-            Assign("x", Add(Number(1), Number(1))),
-            Assign("y", Add(Variable("x"), Number(1))),
-        )
-        Machine(seq, {}, True).run()
+    seq = Sequence(
+        Assign("x", Add(Number(1), Number(1))),
+        Assign("y", Add(Variable("x"), Number(1))),
+    )
+    Machine(seq, {}, True).run()
 
-        print(SEP)
+    print(SEP)
 
-        seq = While(
-            LessThan(Variable("x"), Number(50)),
-            Assign("x", Add(Variable("x"), Number(3))),
-        )
-        Machine(seq, {"x": Number(1)}).run()
+    seq = While(
+        LessThan(Variable("x"), Number(50)),
+        Assign("x", Add(Variable("x"), Number(3))),
+    )
+    Machine(seq, {"x": Number(1)}).run()
 
-        self.assertEqual(eval(seq.to_python)({"x": 1}), {"x": 52})
+    assert eval(seq.to_python)({"x": 1}) == {"x": 52}
+
+    source = """
+        a = 0
+        b = 0
+        while (a < 5) {
+            a = a + 1
+            b = b + a + 1
+        }
+    """
+    seq = parse(source)
+    assert Machine(seq).run().environment, {"a": Number(5), "b": Number(20)}
+    assert eval(seq.to_python)({}), {"a": 5, "b": 20}
+
+    seq = parse(" ")
+    assert seq == DoNothing()
